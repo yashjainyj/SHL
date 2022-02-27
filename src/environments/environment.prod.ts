215bd8b6ae085b7e772cb4af2d57fd96a8ee05b3
yashjainyj <jainyash031@gmail.com>
2022-02-28 00:22:52 +0530

    add backend url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
-  url:"http://localhost:3000/"
+  // url:"http://localhost:3000/"
+  url:'https://studyhomelibrary.herokuapp.com/'
   // url:''
 };
